e2c84a61d6dc01e622694c3afec05ae4a69befc9
Richard Huang <flyerhzm@gmail.com>
2013-03-17 18:15:52 +0800

    generate uuid

--- AppPaoPao/APPHttpClient.m
+++ AppPaoPao/APPHttpClient.m
@@ -11,9 +11,11 @@
 #import <CommonCrypto/CommonDigest.h>
 #import <CommonCrypto/CommonHMAC.h>
 
+#define UUID_USER_DEFAULTS_KEY @"APPPaoPao_UUID"
+
 @implementation APPHttpClient
 
-+(void) sendFeedback
++(void)sendFeedback
 {
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://localhost:9292/feedbacks.json"]];
     NSDictionary *feedbackDict = [[NSDictionary alloc] initWithObjects:@[@"feedback title", @"feedback content"] forKeys:@[@"title", @"content"]];
@@ -25,28 +27,32 @@
     [request setValue:@"application/json" forHTTPHeaderField:@"content-type"];
     [request setValue:@"application/json" forHTTPHeaderField:@"accept"];
     [request setHTTPBody:requestData];
-    [self sign:request];
+    [self prepareHttpHeaders:request];
     
     NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
     [connection start];
 }
 
-+(void) sign:(NSMutableURLRequest *)request
++(void)prepareHttpHeaders:(NSMutableURLRequest *)request
+{
+    [request setValue:[[AppPaoPao sharedConnection] apiKey] forHTTPHeaderField:@"APP_KEY"];
+    [request setValue:[self getUUID] forHTTPHeaderField:@"APP_UUID"];
+    NSString *timestamp = [NSString stringWithFormat:@"%f", [[[NSDate alloc] init] timeIntervalSince1970]];
+    [request setValue:timestamp forHTTPHeaderField:@"APP_TIMESTAMP"];
+    [request setValue:[self getSignature:request timestamp:timestamp] forHTTPHeaderField:@"APP_SIGNATURE"];
+}
+
++(NSString *)getSignature:(NSMutableURLRequest *)request timestamp:(NSString *)timestamp
 {
     NSString *httpMethod = request.HTTPMethod;
     NSString *httpUrl = [request.URL absoluteString];
     NSString *httpBody = [[NSString alloc] initWithData:request.HTTPBody encoding:NSNonLossyASCIIStringEncoding];
-    NSString *timestamp = [NSString stringWithFormat:@"%f", [[[NSDate alloc] init] timeIntervalSince1970]];
     NSString *rawText = [[[httpMethod stringByAppendingString:httpUrl] stringByAppendingString:httpBody] stringByAppendingString:timestamp];
     
-    NSString *signature = [self encodeWithHmacsha1:[[AppPaoPao sharedConnection] apiSecret] rawText:rawText];
-    NSString *apiKey = [[AppPaoPao sharedConnection] apiKey];
-    [request setValue:apiKey forHTTPHeaderField:@"APP_KEY"];
-    [request setValue:timestamp forHTTPHeaderField:@"APP_TIMESTAMP"];
-    [request setValue:signature forHTTPHeaderField:@"APP_SIGNATURE"];
+    return [self encodeWithHmacsha1:[[AppPaoPao sharedConnection] apiSecret] rawText:rawText];
 }
 
-+ (NSString *) encodeWithHmacsha1:(NSString *)key rawText:(NSString*)text
++(NSString *)encodeWithHmacsha1:(NSString *)key rawText:(NSString*)text
 {
     const char *cKey  = [key cStringUsingEncoding:NSASCIIStringEncoding];
     const char *cData = [text cStringUsingEncoding:NSASCIIStringEncoding];
@@ -56,5 +62,21 @@
     CCHmac(kCCHmacAlgSHA1, cKey, strlen(cKey), cData, strlen(cData), cHMAC);
     
     return [NSString  stringWithFormat:@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x", cHMAC[0], cHMAC[1], cHMAC[2], cHMAC[3], cHMAC[4], cHMAC[5], cHMAC[6], cHMAC[7], cHMAC[8], cHMAC[9], cHMAC[10], cHMAC[11], cHMAC[12], cHMAC[13], cHMAC[14], cHMAC[15], cHMAC[16], cHMAC[17], cHMAC[18], cHMAC[19]];
+}
+
++(NSString *)getUUID
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *uuidString = [defaults objectForKey:UUID_USER_DEFAULTS_KEY];
+    if (uuidString == nil) {
+        CFUUIDRef uuid = CFUUIDCreate(kCFAllocatorDefault);
+        NSString *uuidString = (__bridge_transfer NSString *)CFUUIDCreateString(kCFAllocatorDefault, uuid);
+        CFRelease(uuid);
+        
+        [defaults setValue:uuidString forKey:UUID_USER_DEFAULTS_KEY];
+        [defaults synchronize];
+    }
+    
+    return uuidString;
 }
 @end
